e6ac4e7cf9a60fdb05caa69516fc5af926bb8f41
Alexis <oahintz@gmail.com>
2020-11-13 20:09:44 -0300

    -Wip

--- routes/products.js
+++ routes/products.js
@@ -8,7 +8,6 @@ router.get('/', async (req, res) => {
     res.send(prod);
  }); 
 
-
 router.get('/:id(\\d+)', async (req, res) => {
   const prod = await Product.findByPk(req.params.id);
   res.send(prod);
@@ -18,5 +17,9 @@ router.get('/new', (req, res) => {
   res.render('products/new');
 });
 
+router.post('/', async (req, res) => {
+  await Product.create({title: 'pepito' , description: 'narigon'})  
+  res.redirect('/products');
+} )
 
 module.exports = router;
