275eba9c6935c533035bb591adf7261e095bfcf5
Sebastian Stawecki <sstawecki@gmail.com>
2020-11-16 18:51:51 -0300

    Fix heroku db config with use_env_variable

--- models/index.js
+++ models/index.js
@@ -7,9 +7,15 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/db')[env];
 const db = {};
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-fs 
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs
   .readdirSync(__dirname)
   .filter(file => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
